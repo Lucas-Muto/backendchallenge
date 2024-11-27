0eb981c102bfaa47d6b83af737cb6d29b5e22118
unknown <lucasmuto@id.uff.br>
2024-11-27 14:25:46 -0300

    segunda alteração

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ const app = require('express')();
 app.listen(3000, () => console.log('Server running'));
 
 app.get('/skills', (req, res) => {
-    res.send('Javascript and Node');
+    res.send({
+        javascript: 10,
+        node: 10
+    });
 })
