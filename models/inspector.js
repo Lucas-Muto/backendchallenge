0b6bcd020c97933ee76a730a1e289a79251477fd
Lucas Muto <lucasmuto@id.uff.br>
2024-12-01 00:29:54 -0300

    criando modelo do fiscal

---
+++ models/inspector.js
@@ -0,0 +1,9 @@
+class Inspector {
+  constructor(name, email, password) {
+    this.name = name;
+    this.email = email;
+    this.password = password;
+  }
+}
+
+module.exports = Inspector; 
---
+++ prisma/migrations/20241201032904_criar_fiscal/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "Inspector" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Inspector_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Inspector_email_key" ON "Inspector"("email");
